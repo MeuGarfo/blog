db640f7dd3528732d3aad83f8703cd111d421e91
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-01-09 20:29:29 -0200

    M  view/posts/read.php

--- view/posts/read.php
+++ view/posts/read.php
@@ -5,7 +5,7 @@ if ($user) {
     $url='/posts/'.$post['slug'].'/'.$post['id'].'?update';
     $updateLink='<a href="'.$url.'">Editar</a> | ';
 }
-$postCreatedAt=strftime("%d/%b/%Y %H:%M", $post['created_at']);
+$postCreatedAt=strftime("%d/%b/%Y %H:%M:%S", $post['created_at']);
 $postCreatedAt=ucfirst($postCreatedAt);
 $data['content']=<<<heredoc
 <p class="left"><small>{$updateLink}{$postCreatedAt}</small></p>
