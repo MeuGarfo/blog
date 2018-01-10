4763d50701f95ce8023db8a7629ace78f51e1c3e
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-01-09 22:21:35 -0200

    M  view/layout.php

--- view/layout.php
+++ view/layout.php
@@ -17,10 +17,10 @@ if (!isset($_ENV['site_name'])) {
     <meta charset="utf-8">
     <title><?php print $title; ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/asset/bower_components/1k/dist/1k.min.css">
-    <link rel="stylesheet" href="/asset/css/main.css">
-    <script src="/asset/js/main.js"></script>
-    <script src="/asset/js/zepto.min.js"></script>
+    <link rel="stylesheet" href="/asset/bower_components/1k/dist/1k.min.css?2.0.3">
+    <link rel="stylesheet" href="/asset/css/main.css?0.1.0">
+    <script src="/asset/js/main.js?0.1.2"></script>
+    <script src="/asset/js/zepto.min.js?1.2.0"></script>
     <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="/feed/" />
     <meta property="og:site_name" content="<?php print $_ENV['site_name']; ?>">
     <meta property="og:title" content="<?php print $title; ?>">
