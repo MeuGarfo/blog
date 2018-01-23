11c6662c629833825149befe43e4615537fbe182
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-01-23 15:40:08 -0200

    M  view/inc/filesCreate.php

--- view/inc/filesCreate.php
+++ view/inc/filesCreate.php
@@ -1,5 +1,14 @@
 <h2><?php print $title; ?></h2>
 <form action="/files?create" enctype="multipart/form-data" method="post">
-    <input name="file" type="file">
+    <input id="file" name="file" type="file">
     <button type="submit"><?php print $title; ?></button>
 </form>
+<script type="text/javascript">
+function send_data() {
+    document.forms[0].submit();
+}
+window.onload = function(){
+    var input = document.getElementById('file');
+    input.onchange = send_data;
+}
+</script>
