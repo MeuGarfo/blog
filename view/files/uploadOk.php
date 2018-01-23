5391aec290097b51a4afbc15c312177b2fdcbbf9
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-01-23 15:38:21 -0200

    M  view/files/uploadOk.php

--- view/files/uploadOk.php
+++ view/files/uploadOk.php
@@ -3,6 +3,7 @@ $data['title']='Sucesso';
 if ($file['is_image']) {
     $data['content']=<<<heredoc
 <h2>{$data['title']}</h2>
+<p><a href="/files/?create">Enviar outro arquivo</a></p>
 <textarea rows="3">
     <a href="/images/cover/{$file['name']}" title="Ver o original">
         <img src="/images/400/{$file['name']}" alt="{$file['name']}">
