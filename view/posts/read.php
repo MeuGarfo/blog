7f2c712d083bfe9fb52fc2e313f60f7e17d4147a
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-02-06 16:34:48 -0200

    M  view/posts/read.php

--- view/posts/read.php
+++ view/posts/read.php
@@ -9,21 +9,11 @@ if ($user) {
 $postCreatedAt=strftime("%d/%b/%Y %H:%M:%S", $post['created_at']);
 $postCreatedAt=ucfirst($postCreatedAt);
 $data['content']=<<<heredoc
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.11';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
 <p class="left"><small>{$updateLink}{$postCreatedAt}</small></p>
 <h2>{$post['title']}</h2>
 <div class="content" id="post">
 {$post['content']}
-</div>
-<div id="comments" class="center desktop">
-<div data-width="100%" class="fb-comments" data-href="{$_ENV['site_url']}{$shareLink}" data-numposts="5"></div>
+<h3 class="center">Gostou? Compartilhe!</h3>
 </div>
 heredoc;
 $view->view('layout', $data);
