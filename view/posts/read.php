ffceca433314569f3d64b4318104f8653a0095d8
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-03-12 15:16:07 -0300

    M  view/posts/read.php

--- view/posts/read.php
+++ view/posts/read.php
@@ -9,11 +9,21 @@ if ($user) {
 $postCreatedAt=strftime("%d/%b/%Y %H:%M:%S", $post['created_at']);
 $postCreatedAt=ucfirst($postCreatedAt);
 $data['content']=<<<heredoc
-<p class="left"><small>{$updateLink}{$postCreatedAt}</small></p>
+<p class="right"><small>{$updateLink}{$postCreatedAt}</small></p>
 <h2>{$post['title']}</h2>
 <div class="content" id="post">
 {$post['content']}
-<p class="left"><small>{$updateLink}{$postCreatedAt}</small></p>
+<p class="right"><small>{$updateLink}{$postCreatedAt}</small></p>
+<div class="center">
+    <a href="https://facebook.com/groups/{$_ENV['fb_group']}" title="Ir para o Grupo">
+        <img src="/images/560/facebook.png" alt="facebook.png">
+    </a><br>
+    <small>
+    <a href="https://facebook.com/groups/{$_ENV['fb_group']}">
+        https://facebook.com/groups/{$_ENV['fb_group']}
+    </a>
+    </small>
+</div>
 <h3 class="center">Compartilhar</h3>
 </div>
 heredoc;
