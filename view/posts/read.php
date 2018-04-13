b53d98ac3d7c85bc923ace561bb3a6177614a2b4
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-04-13 16:54:42 -0300

    M  view/posts/read.php

--- view/posts/read.php
+++ view/posts/read.php
@@ -13,7 +13,7 @@ $data['content']=<<<heredoc
 <h2>{$post['title']}</h2>
 <div class="center">
 <h3>Compartilhe</h3>
-<div class="center addshare"></div>
+<div id="addshare"></div>
 </div>
 <div class="content" id="post">
 {$post['content']}
