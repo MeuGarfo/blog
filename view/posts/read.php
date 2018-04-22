39447e8e4dc3efa3cf05d17b60f2551b7410299c
Anderson Ismael <aicoutos@users.noreply.github.com>
2018-04-21 21:56:44 -0300

    M  view/posts/read.php

--- view/posts/read.php
+++ view/posts/read.php
@@ -19,7 +19,6 @@ $data['content']=<<<heredoc
     <a href="https://facebook.com/{$_ENV['fb_page']}" title="Ir para a Página" target="_blank">
         <img src="/images/560/curtir.png" alt="Ir para a Página" width="560" height="172" style="width:560px;height:auto;">
     </a>
-<h3>Deixe seu comentário</h3>
 <div id="disqus_thread"></div>
 <script>
 
